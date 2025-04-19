39af9a9cad9ff8bd5987d872363936d06fab4608
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-04-19 15:12:59 +0600

    solved matrix problem..modiule-18.5

---
+++ week_01/day_09/matrix.py
@@ -0,0 +1,20 @@
+
+# time and space complexity -----> O(n^2)
+
+n = int(input())
+matrix = []
+for i in range(n):
+    row = list(map(int,input().split()))
+    matrix.append(row)
+sum1 = 0
+sum2 = 0
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            sum1 += matrix[i][j]
+            
+        if i+j==n-1:
+            sum2 += matrix[i][(n-i)-1]
+
+result = (sum1-sum2)* -1
+print(result)
