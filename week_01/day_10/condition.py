1833b311e74eee12e4dfce16a2a9524546fcf97c
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-04-20 14:33:36 +0600

    solved condition problem ..modiule-19.5

---
+++ week_01/day_10/condition.py
@@ -0,0 +1,13 @@
+
+# time and space complexity ----- > O(1)
+a,b,k = map(int,input().split())
+
+
+if a % k == 0 and b % k == 0:
+    print('Both')
+elif a % k == 0:
+    print('Memo')
+elif b % k == 0:
+    print('Momo')
+else:
+    print('No One')
