793ea190c9807338326a71fe874ecde2b675e1ff
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-04-23 16:14:37 +0600

    try to solved this problem but codeforeces show an error memory limit exiteed but my approach is right for c,c++ or java language..

--- week_02/day_01/sort_string.py
+++ week_02/day_01/sort_string.py
@@ -3,11 +3,19 @@
 n = int(input())
 s = input()
 
-count = {chr(ch):0 for ch in range(ord('a'),ord('z'))}
-for char in s:
-    count[char] += 1
+count = [0]*26
 
-for key,value in count.items():
+for char in s:
+    count[ord(char)-ord('a')]+=1
+
+for i in range(26):
+    print(chr(i+ord('a')) * count[i] , end='')
+
+# count = {chr(ch):0 for ch in range(ord('a'),ord('z'))}
+# for char in s:
+#     count[char] += 1
+
+# for key,value in count.items():
     
-    if value>0:
-        print(key*value,end='')
+#     if value>0:
+#         print(key*value,end='')
