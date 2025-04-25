e905f994362abec2b2912e3a53ef581515a0cc2d
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-04-25 20:12:50 +0600

    solved icpc problem ..modiule--6.5

---
+++ week_02/day_02/icpc_ballons.py
@@ -0,0 +1,21 @@
+
+
+# time complexity ---- > O(t*n)
+# space complexity -----> O(n)
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input()
+    count = [0]*26
+
+    for char in s:
+        count[ord(char) - ord('A')] += 1
+
+    total_bellon = 0
+
+    for i in range(26):
+        if count[i]>=1:
+            total_bellon = total_bellon + 2 + (count[i]-1)
+
+    print(total_bellon)
