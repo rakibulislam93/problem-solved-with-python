99d57ab1b6a173f6c1653ca03df14a256d947c76
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-04-29 21:05:03 +0600

    Insert value at the head single link list..

---
+++ Data Structure/day_01/insert_at_head.py
@@ -0,0 +1,82 @@
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+
+def insert_at_tail(head,value):
+    newNode = Node(value)
+    if head == None:
+        head = newNode
+        return head
+
+    temp = head
+    while temp.next != None:
+        temp = temp.next 
+    
+    temp.next = newNode
+    
+    return head
+
+def show_list(head):
+    if head is None:
+        print('head none ache..')
+    temp = head
+    while temp != None:
+        print(temp.data,end=' ')
+        temp = temp.next 
+    
+    print()
+
+def insert_at_position(head,pos,value):
+    newNode = Node(value)
+    if pos==0:
+        newNode.next = head
+        head = newNode
+        return head
+    
+    temp = head
+    for i in range(pos-1):
+        temp = temp.next 
+    
+    newNode.next = temp.next 
+    temp.next = newNode
+    
+    return head
+    
+
+head = None
+
+# head = insert_at_tail(head, 10)
+# head = insert_at_tail(head, 20)
+# head = insert_at_tail(head, 30)
+# head = insert_at_tail(head, 40)
+
+# show_list(head)
+# insert_at_position(head,2,500)
+# show_list(head)
+
+while True:
+    print('1. Insert value at tail')
+    print('2. Show link list')
+    print('3. Insert value at position ')
+    print('4. exit')
+    
+    choice = input()
+    if choice == '1':
+        value = int(input('Enter the value : '))
+        head = insert_at_tail(head,value)
+        
+    elif choice == '2':
+        
+        show_list(head)
+        
+    elif choice == '3':
+        pos = int(input('Enter the position : '))
+        value = int(input('Enter the value : '))
+        head = insert_at_position(head,pos,value)
+    
+    else:
+        break
+        
