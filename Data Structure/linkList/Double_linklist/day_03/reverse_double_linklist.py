9abf9883deaf5ef81608ba3741d489f7e6c9d63e
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-05-15 14:43:30 +0600

    print double linklist reveseway..modiule--10.5

---
+++ Data Structure/linkList/Double_linklist/day_03/reverse_double_linklist.py
@@ -0,0 +1,34 @@
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+def insert_at_tail(head,tail,value):
+    newNode = Node(value)
+    if head is None:
+        head = newNode
+        tail = newNode
+        return head,tail 
+    tail.next = newNode
+    newNode.prev = tail
+    tail = newNode
+    return head,tail
+
+def print_linklist_reverse(tail):
+    temp = tail
+    while temp is not None:
+        print(temp.value,end=' ')
+        temp = temp.prev 
+    print()
+
+head = None
+tail = None
+linklist = list(map(int,input().split()))
+for value in linklist:
+    if value == -1:
+        break
+    head, tail = insert_at_tail(head,tail,value)
+
+print_linklist_reverse(tail)
