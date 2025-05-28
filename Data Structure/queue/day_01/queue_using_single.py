4107046e6af9bba5ce614349792302837b3af1f1
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-05-28 15:47:39 +0600

    implement queue using single linklist..modiule--13

---
+++ Data Structure/queue/day_01/queue_using_single.py
@@ -0,0 +1,48 @@
+
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+        
+
+class myQueue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.sz = 0
+    
+    def push(self,value):
+        self.sz +=1
+        newNode = Node(value)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+            return
+        
+        self.tail.next = newNode
+        self.tail = newNode
+    
+    def pop(self):
+        if not self.empty():
+            self.sz -=1
+            delNode = self.head 
+            self.head = delNode.next
+            del delNode
+    
+    def top(self):
+        if not self.empty():
+            return self.head.value
+        return None
+    
+    def empty(self):
+        return self.sz == 0
+
+queue = myQueue()
+new_queue = list(map(int,input().split()))
+
+for value in new_queue:
+    queue.push(value)
+    
+while not queue.empty():
+    print(queue.top(),end=" ")
+    queue.pop()
