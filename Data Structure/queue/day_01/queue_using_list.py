0ca59be7277583382abeb0f74b53735779e84d69
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-05-29 15:08:23 +0600

    implement queue using built in method list..modiule--13

---
+++ Data Structure/queue/day_01/queue_using_list.py
@@ -0,0 +1,11 @@
+
+queue = []
+queue.append(10)
+queue.append(20)
+queue.append(30)
+print('Initial time queue : ',queue)
+
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+print("After pop all element : ", queue)
