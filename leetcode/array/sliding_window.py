3ed851fd91c3c678309a27b798fbe2eb64a8c02e
rakibulislam93 <rakibulislamarif793@gmail.com>
2025-10-05 13:46:18 +0600

    find maximum sum and subarray using sliding window technic

--- leetcode/array/sliding_window.py
+++ leetcode/array/sliding_window.py
@@ -4,9 +4,22 @@ k = 3
 window_sum = sum(arr[:3])
 max_sum = window_sum
 
+sub_arr = arr[:3]
+
+
+# for i in range(k,len(arr)):
+#     window_sum += arr[i]-arr[i-k]
+#     max_sum = max(max_sum,window_sum)
+
 for i in range(k,len(arr)):
     window_sum += arr[i]-arr[i-k]
-    max_sum = max(max_sum,window_sum)
+    if window_sum > max_sum:
+        max_sum = window_sum
+        sub_arr = arr[i-k+1:i+1]
 
 
+
+print(sub_arr)
 print(max_sum)
+
+
