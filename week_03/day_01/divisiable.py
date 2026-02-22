d17dc48b3f473607004c1411104ad99231a20510
Rakibul Islam <rakibulislamarif793@gmail.com>
2026-02-23 01:07:22 +0600

    solved divisiable problem and shoping related problem where using if-else

---
+++ week_03/day_01/divisiable.py
@@ -0,0 +1,3 @@
+
+n = int(input("Enter a number: "))
+print("YES" if n %3 ==0 else "NO")
---
+++ week_03/day_01/shoping.py
@@ -0,0 +1,10 @@
+
+n = int(input("Enter a number: "))
+
+if n <= 1000:
+    print("Bad luck!")
+
+else:
+    print("I will buy a panjabi")
+    if n >= 1500:
+        print("I will buy a pair of shoes\nAlisa will buy new shoes")
